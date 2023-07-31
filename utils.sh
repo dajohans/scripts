93110350be26a78cb0e2cb7df4bc7aedc54583ec
David Johansson <davidjohansson1994@gmail.com>
2023-07-31 18:09:18 +0300

    Fix: source rust after installation, for using rustup immediately

--- utils.sh
+++ utils.sh
@@ -64,6 +64,7 @@ rust_install () {
 	if [[ $(command_exists rustup) = false ]]
 	then
 		curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | bash -s -- -y
+		source $HOME/.cargo/env
 	fi
 }
 
