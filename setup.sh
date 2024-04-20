dce0099acd37cdfe14fe2293d1df6ba1cfeae4ca
David Johansson <davidjohansson1994@gmail.com>
2024-04-20 17:46:17 +0300

    Make ssh-keygen dynamically find user/hostname, instead of hardcoded

--- setup.sh
+++ setup.sh
@@ -78,7 +78,8 @@ ssh_key_filename=$HOME/.ssh/id_ed25519
 ssh_key_passphrase=KL098E10
 if [[ $(file_exists "$ssh_key_filename") = false ]]
 then
-	ssh-keygen -t ed25519 -C "djohansson@L5P" -f $ssh_key_filename -N "$ssh_key_passphrase"
+	hostname=$(uname -n)
+	ssh-keygen -t ed25519 -C "$USER@$hostname" -f $ssh_key_filename -N "$ssh_key_passphrase"
 	eval "$(ssh-agent -s)"
 	ssh-add $ssh_key_filename
 	# Add the key to github here: https://github.com/settings/keys
